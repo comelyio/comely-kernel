4505f15781a0795f1dc164e35f208a8437063bc4
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-08-20 15:52:18 +0500

    Exception update

--- Exception/ComelyException.php
+++ Exception/ComelyException.php
@@ -14,10 +14,22 @@ declare(strict_types=1);
 
 namespace Comely\Kernel\Exception;
 
+use Throwable;
+
 /**
  * Class ComelyException
  * @package Comely\Kernel\Exception
  */
 class ComelyException extends \Exception
 {
+    /**
+     * ComelyException constructor.
+     * @param string $message
+     * @param int|null $code
+     * @param null|Throwable $previous
+     */
+    public function __construct(string $message = "", ?int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, intval($code), $previous);
+    }
 }
